860b5dafc19ee8c78b57a08e2484157a6c1dc519
Danny Pineda and Heather McCarthy <newwavehealing@gmail.com>
2016-03-30 20:01:23 -0700

    Add day two initial files and early scripts attempt to set up Board object.

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+var b = ((0,0,0), (0,0,0), (0,0,0));
+
+function Board(row1, row2, row3) {
+  var row1 = (0, 0, 0);
+  var row2 = (0, 0, 0);
+  var row3 = (0, 0, 0);
+}
+function Checkspace(x,y){
+  b[x][y] return b[x][y];
+}
+
+
+
+
+
+$(document).ready(function() {
+  $(".square").click(function(event) {
+    $(this).text("X");
+  });
+
+    event.preventDefault();
+});
