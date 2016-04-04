f3cb3f4ff44960577f68e476acf4b3336f8858af
Heather <newwavehealing@gmail.com>
2016-04-04 14:24:37 -0700

    Update scripts.js with board constructor and object

--- js/scripts.js
+++ js/scripts.js
@@ -1,22 +1,68 @@
-var b = ((0,0,0), (0,0,0), (0,0,0));
+var b = [[0,0,0], [0,0,0], [0,0,0]];
 
-function Board(row1, row2, row3) {
-  var row1 = (0, 0, 0);
-  var row2 = (0, 0, 0);
-  var row3 = (0, 0, 0);
-}
-function Checkspace(x,y){
-  b[x][y] return b[x][y];
+function Player(playerNumber,name) {
+  this.playerNumber = playerNumber;
+  this.name = name;
 }
 
+function Board(firstPlayer) {
+  this.nextPlayer = firstPlayer.playerNumber;
+}
 
+Board.prototype.markSpace = function(x, y, playerNumber) {
+  console.log("Meow!");
+  //debugger;
+  if(this.checkSpace(x, y)===0) {
+    // first mark space taken in our array.
+    b[x][y] = playerNumber;
+    // now draw on the screen
+    $("#space"+x+"-"+y).text("X");
+  } else {
+    // space is already taken!
+    console.log(x+","+y+" Space already taken by player "+playerNumber);
+  }
+}
 
+Board.prototype.checkSpace = function(x, y) {
+  console.log("checkSpace!" + b[x][y]);
+  return b[x][y];
+}
 
+/*
+var Board = {
+  nextPlayer: 1,
+  markSpace: function(x, y, playerNumber) {
+    console.log("Meow!");
+    if(this.checkSpace(x, y)===0) {
+      // first mark space taken in our array.
+      b[x][y] = playerNumber;
+      // now draw on the screen
+      $(this).text("X");
+    } else {
+      // space is already taken!
+      console.log(x+","+y+" Space already taken by player "+playerNumber);
+    }
+  },
+  checkSpace: function(x, y) {
+    console.log("checkSpace!" + b[x][y]);
+    return b[x][y];
+  }
+};
+*/
 
+// start of tic tac toe game.
 $(document).ready(function() {
+
+  var p1 = new Player(1,"X");
+  var p2 = new Player(2,"O");
+
+  // create board and set first player.
+  var board = new Board(p1);
+
   $(".square").click(function(event) {
-    $(this).text("X");
+    console.log("square clicked!");
+    board.markSpace(0,0,1);
   });
 
-    event.preventDefault();
+  //event.preventDefault();
 });
